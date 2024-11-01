1ae6fd5834520be598222edf84ddb412ccffd444
Simon0824 <szymon.szostek008@gmail.com>
2024-11-01 17:02:07 +0100

    Dodanie komentarzy

--- Iloscwlosow/MainWindow.xaml.cs
+++ Iloscwlosow/MainWindow.xaml.cs
@@ -11,41 +11,38 @@ using System.Windows.Shapes;
 
 namespace Iloscwlosow
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
-        private const double domyslnailoscwlosow = 150000;
+        private const double domyslnailoscwlosow = 150000; //ustawienie domyslnej ilosc włosów, która będzie porównywana z ilością włosów użytkownika
         public MainWindow()
         {
             InitializeComponent();
         }
-        
-private void Obliczaniewlosow(object sender, RoutedEventArgs e)
+
+        private void Obliczaniewlosow(object sender, RoutedEventArgs e) // obliczanie liczby włosów przez wprowadzone dane przez użytkownika
         {
-            if (double.TryParse(Gestosc.Text, out double gestosc) &&
+            if (double.TryParse(Gestosc.Text, out double gestosc) && // zmiana tekstu na liczby zmiennoprzecinkowe
                             double.TryParse(obwod.Text, out double obw) &&
                             double.TryParse(wysokosc.Text, out double wys))
             {
-                double radius = obw / (2 * Math.PI);
-                double powierzchnia = 4 * Math.PI * Math.Pow(radius, 2);
+                double promien = obw / (2 * Math.PI); // oblicza promień
+                double powierzchnia = 4 * Math.PI * Math.Pow(promien, 2); //oblicza powierzchnie głowy
 
 
-                double liczbawlosow = gestosc * powierzchnia;
+                double liczbawlosow = gestosc * powierzchnia; // oblicza liczbę włosow przez podaną liczbe włosów na cm2 oraz obliczoną wcześniej powierzchnie
 
-                double roznicaprocent = ((liczbawlosow - domyslnailoscwlosow) / domyslnailoscwlosow) * 100;
+                double roznicaprocent = ((liczbawlosow - domyslnailoscwlosow) / domyslnailoscwlosow) * 100; //oblicza rożnice między obliczoną ilością włosów a średnią w procentach
 
-                Wynik.Text = $"Twoja przybliżona Liczba włosów:\n{liczbawlosow:F0} ({roznicaprocent:F2}% w stosunku do średniej)";
+                Wynik.Text = $"Twoja przybliżona Liczba włosów:\n{liczbawlosow:F0} ({roznicaprocent:F2}% w stosunku do średniej)"; //wyświetlenie wyniku oraz róznicy
             }
             else
             {
-                MessageBox.Show("Proszę wpropwadzić prawidłowe dane.", "Błąd", MessageBoxButton.OK, MessageBoxImage.Error);
+                MessageBox.Show("Proszę wpropwadzić prawidłowe dane.", "Błąd", MessageBoxButton.OK, MessageBoxImage.Error); //wyskakuje błąd jeśli użytkownik nie wprowadził danych
             }
         }
-       
 
-        private void Reset(object sender, RoutedEventArgs e)
+
+        private void Reset(object sender, RoutedEventArgs e) //działanie przycisku resetującego obliczenia
         {
             Gestosc.Text = string.Empty;
             obwod.Text = string.Empty;
