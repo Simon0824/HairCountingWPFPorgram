9eded981564ee5e29409b33398cf8097b4883578
Simon0824 <szymon.szostek008@gmail.com>
2024-11-01 19:19:39 +0100

    Dodanie klasy głowa

--- Iloscwlosow/MainWindow.xaml.cs
+++ Iloscwlosow/MainWindow.xaml.cs
@@ -13,10 +13,68 @@ namespace Iloscwlosow
 {
     public partial class MainWindow : Window
     {
+        public class Glowa //klasa głowa reprezentuje model głowy oraz zawiera metody  i właściwości niezbedne do obliczeń powierzchni głowy i liczby włosów
+        {
+            private const double SREDNIA_GESTOSC = 150; //ustawienie domyślnej średniej gęstości włosów
+            private const double SREDNI_OBWOD = 56; //ustawienie średniego obwodu głowy
+            private const double SREDNIA_WYSOKOSC = 7; //ustawienie średniej wysokości czoła
+
+            //zmienne do przechowywania wartośći domyślnych powyżej
+            private double gest; 
+            private double obwodglowy;
+            private double wysok;
+            private double powierzchniaglowy;
+
+            public Glowa() //konstruktor Glowa nadający wartości domyślne
+            {
+                gest = SREDNIA_GESTOSC;
+                obwodglowy = SREDNI_OBWOD;
+                wysok = SREDNIA_WYSOKOSC;
+                UstawPowierzchnieGlowy(); //wywołanie metody obliczającą powierzchnie głowy
+            }
+
+            public void UstawGestosc(double gest) //ustawienie gęstości włosów
+            {
+                this.gest = gest;
+            }
+            public void UstawObwod(double obwodglowy) //ustawienie obwodu głowy
+            {
+                this.obwodglowy = obwodglowy;
+                UstawPowierzchnieGlowy();
+            }
+
+            public void UstawWysokoscCzola(double wysok) //ustawienie wysokości czoła
+            {
+                this.wysok = wysok;
+                UstawPowierzchnieGlowy();
+            }
+
+            public void UstawPowierzchnieGlowy() //obliczenie powierzchni głowy
+            {
+                double promien = obwodglowy / (2 * Math.PI);
+                powierzchniaglowy = 4 * Math.PI * Math.Pow(promien, 2);
+            }
+
+            public double ObliczLiczbeWlosow() //obliczenie liczby włosów
+            {
+                return gest * powierzchniaglowy;
+            }
+
+            public double ObliczliczbeWlosow(double gestosc, double obwod, double wysokoscCzola) //obliczenie liczby włosów na nowych danych wejściowych
+            {
+                double promien = obwodglowy / (2 * Math.PI);
+                double powierzchniaglowy = 4 * Math.PI * Math.Pow(promien, 2);
+                return gest * powierzchniaglowy;
+            }
+        }
+        private Glowa glowa;
+
+
         private const double domyslnailoscwlosow = 150000; //ustawienie domyslnej ilosc włosów, która będzie porównywana z ilością włosów użytkownika
         public MainWindow()
         {
             InitializeComponent();
+            glowa = new Glowa();
         }
 
         private void Obliczaniewlosow(object sender, RoutedEventArgs e) // obliczanie liczby włosów przez wprowadzone dane przez użytkownika
@@ -25,11 +83,11 @@ namespace Iloscwlosow
                             double.TryParse(obwod.Text, out double obw) &&
                             double.TryParse(wysokosc.Text, out double wys))
             {
-                double promien = obw / (2 * Math.PI); // oblicza promień
-                double powierzchnia = 4 * Math.PI * Math.Pow(promien, 2); //oblicza powierzchnie głowy
+                glowa.UstawGestosc(gestosc);
+                glowa.UstawObwod(obw);
+                glowa.UstawWysokoscCzola(wys);
 
-
-                double liczbawlosow = gestosc * powierzchnia; // oblicza liczbę włosow przez podaną liczbe włosów na cm2 oraz obliczoną wcześniej powierzchnie
+                double liczbawlosow = glowa.ObliczLiczbeWlosow();
 
                 double roznicaprocent = ((liczbawlosow - domyslnailoscwlosow) / domyslnailoscwlosow) * 100; //oblicza rożnice między obliczoną ilością włosów a średnią w procentach
 
